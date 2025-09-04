33b9b5251d7b4c168473c6a4d203ef02195179ed
Rokibq1 <rokibmusa13@gmail.com>
2025-09-04 12:41:38 +0100

    warehouse

---
+++ script.js
@@ -0,0 +1,13 @@
+// Welcome to JS
+console.log("Hello");
+console.log(2+4);
+  
+//perimeter
+var length =10;
+const dateOfBirth = "01/18/2009";
+console.log("perimeter:",4*length);
+console.log("Area:",length*length);
+
+var age=25;
+
+console.log(age*2);
